97952b821d72a5f5a99f417526724cdf42e40cc5
Wojciech Brzozowy <woj.brzozowy@gmail.com>
2020-03-22 08:27:31 +0100

    registration and login complet

--- donation/forms.py
+++ donation/forms.py
@@ -1,28 +1,28 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from donation_auth.models import User
+# from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+#
+#
+# class UserLoginForm(AuthenticationForm):
+#     email = forms.EmailField(widget=forms.EmailInput(
+#         attrs={'placeholder': 'Email', 'autofocus': True}))
+#     password = forms.CharField(widget=forms.PasswordInput(
+#         attrs={'placeholder': 'Password', 'autocomplete': 'current-password'}))
 
 
-class UserLoginForm(AuthenticationForm):
-    username = forms.EmailField(widget=forms.EmailInput(
-        attrs={'placeholder': 'Email', 'autofocus': True}))
-    password = forms.CharField(widget=forms.PasswordInput(
-        attrs={'placeholder': 'Hasło', 'autocomplete': 'current-password'}))
-
-
-class RegisterForm(UserCreationForm):
-    username = forms.EmailField(widget=forms.EmailInput(
-        attrs={'placeholder': 'Email', 'autofocus': True}))
-    password1 = forms.CharField(
-        strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
-                                          'placeholder': 'Hasło'}),
-    )
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}),
-    )
-
-    class Meta:
-        model = User
-        fields = ("username", "password1", "password2")
+# class RegisterForm(UserCreationForm):
+#     email = forms.EmailField(widget=forms.EmailInput(
+#         attrs={'placeholder': 'Email', 'autofocus': True}))
+#     password1 = forms.CharField(
+#         strip=False,
+#         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
+#                                           'placeholder': 'Hasło'}),
+#     )
+#     password2 = forms.CharField(
+#         widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}),
+#     )
+#
+#     class Meta:
+#         model = User
+#         fields = ("email", "password1", "password2")
 
