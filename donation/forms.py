80b3b92244b37f9ab1f21f574e217341e5baab51
Wojciech Brzozowy <woj.brzozowy@gmail.com>
2020-03-28 17:50:13 +0100

    user profil data edition finish

--- donation/forms.py
+++ donation/forms.py
@@ -1,40 +1,8 @@
-from django import forms
-from donation.models import Donation
+from django.contrib.auth.forms import PasswordChangeForm
+from donation_auth.models import User
 
 
-# class AddDonationForm(forms.ModelForm):
-#     class Meta:
-#         model = Donation
-#         fields = '__all__'
-
-
-
-
-# from donation_auth.models import User
-# from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-#
-#
-# class UserLoginForm(AuthenticationForm):
-#     email = forms.EmailField(widget=forms.EmailInput(
-#         attrs={'placeholder': 'Email', 'autofocus': True}))
-#     password = forms.CharField(widget=forms.PasswordInput(
-#         attrs={'placeholder': 'Password', 'autocomplete': 'current-password'}))
-
-
-# class RegisterForm(UserCreationForm):
-#     email = forms.EmailField(widget=forms.EmailInput(
-#         attrs={'placeholder': 'Email', 'autofocus': True}))
-#     password1 = forms.CharField(
-#         strip=False,
-#         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password',
-#                                           'placeholder': 'Hasło'}),
-#     )
-#     password2 = forms.CharField(
-#         widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}),
-#     )
-#
-#     class Meta:
-#         model = User
-#         fields = ("email", "password1", "password2")
-
-
+class UserEditForm(PasswordChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
